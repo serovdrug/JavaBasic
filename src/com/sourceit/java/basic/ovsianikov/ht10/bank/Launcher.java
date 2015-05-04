0290c3cf705041248714632c51744fc22b69c219
serovdrug <serovdrug@gmail.com>
2015-05-05 01:40:37 +0300

    Change variables

--- src/com/sourceit/java/basic/ovsianikov/ht10/bank/Launcher.java
+++ src/com/sourceit/java/basic/ovsianikov/ht10/bank/Launcher.java
@@ -10,26 +10,27 @@ public class Launcher {
 		client1.createBankAcc();
 		client2.createBankAcc();
 		
-		int sizeMoney = 100;
+		int creditSizeMoney = 100;
 
-		System.out.println("Client1 get credit - " + sizeMoney + "$\nSaldo = " + client1.getMoney(sizeMoney) + "$");
+		System.out.println("Client1 get credit - " + creditSizeMoney + "$\nSaldo = " + client1.getMoney(creditSizeMoney) + "$");
 		
-		sizeMoney = 150;
-		System.out.println("Client1 put " + sizeMoney + "$\nSaldo = " + client1.putMoney(sizeMoney) + "$");
+		int putSizeMoney = 150;
+		System.out.println("Client1 put " + putSizeMoney + "$\nSaldo = " + client1.putMoney(putSizeMoney) + "$");
 
 		System.out.println("Client1 saldo = " + client1.getSaldo() + "$");
 		System.out.println("Client2 saldo = " + client2.getSaldo() + "$");
 		
-		sizeMoney = 20;
+		int sendSizeMoney = 20;
 
-		client1.sendMoney(client1, client2, sizeMoney);
-		System.out.println("Client1 sent " + sizeMoney + "$ to Client2");
+		client1.sendMoney(client1, client2, sendSizeMoney);
+		System.out.println("Client1 sent " + sendSizeMoney + "$ to Client2");
 		
 		System.out.println("Client1 saldo = " + client1.getSaldo() + "$");
 		System.out.println("Client2 saldo = " + client2.getSaldo() + "$");
 		
-		client2.getMoney(10);
-		System.out.println("Client2 get " + sizeMoney + "$");
+		int getSizeMoney = 5;
+		client2.getMoney(getSizeMoney);
+		System.out.println("Client2 get " + getSizeMoney + "$");
 		
 
 		System.out.println("Client1 saldo = " + client1.getSaldo() + "$");
